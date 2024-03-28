a0514042e8ea436d9c9ec2b8346d3294dd5a8ef2
imlnr <imlnr2002@gmail.com>
2024-03-28 16:47:42 +0530

    final commit

--- src/pages/GroceryList.jsx
+++ src/pages/GroceryList.jsx
@@ -5,7 +5,13 @@ import GroceryItems from '../components/GroceryItems';
 import "../styles/Grocery.css"
 
 const GroceryList = () => {
+  const [visible, setformVisible] = useState(false);
   const [groceries, setGroceries] = useState([]);
+  const [name, setname] = useState('');
+  const [imglink, setimglink] = useState('');
+  const [quantity, setquantity] = useState(0);
+  const [price, setprice] = useState(0);
+  const [desc, setdesc] = useState('');
 
   const deleteGrocery = async (id) => {
     try {
@@ -28,13 +34,33 @@ const GroceryList = () => {
   useEffect(() => {
     fetchGroceries();
   }, []);
-  const [visible, setformVisible] = useState(false);
   // useEffect(() => {
-  //   fetchGroceries();
-  // }, [deleteGrocery])
-
-  return (
-    <>
+    //   fetchGroceries();
+    // }, [deleteGrocery])
+    
+    const postGrocery = async (e) => {
+      e.preventDefault();
+      try {
+        const res = await axios.post('https://grocery-server-geqm.onrender.com/groceries', {
+          name: name,
+          qty: quantity,
+          price: price,
+          description: desc,
+          image: imglink
+        })
+        console.log(res);
+        if (res.status===201) {
+          setformVisible(false);
+          setGroceries((prev)=>[...prev,res.data])
+          // fetchGroceries();
+        }
+        console.log("added Successfully...");
+      } catch (error) {
+        console.error('Error', error);
+      }
+    }
+    return (
+      <>
       <div className={`grocery-main ${visible ? 'blur-background' : ''}`}>
         <h1>Grocery List</h1>
         <button onClick={(e) => setformVisible(!visible)} className='add-grocery'><span>+</span>Add Items</button>
@@ -45,22 +71,22 @@ const GroceryList = () => {
         </div>
       </div>
       {visible && <div className='add-form'>
-        <form>
+        <form onSubmit={postGrocery}>
           <span onClick={(e) => setformVisible(false)}>X</span>
           <div>
-            <input type="text" placeholder='Enter the Grocery Name' required />
+            <input type="text" onChange={(e) => setname(e.target.value)} placeholder='Enter the Grocery Name' required />
           </div>
           <div>
-            <input type="url" placeholder='Enter the image link' required />
+            <input type="url" onChange={(e) => setimglink(e.target.value)} placeholder='Enter the image link' required />
           </div>
           <div>
-            <input type="number" placeholder='Enter the Quantity' />
+            <input type="number" onChange={(e) => setquantity(e.target.value)} placeholder='Enter the Quantity' />
           </div>
           <div>
-            <input type="text" placeholder='Enter the price' />
+            <input type="text" onChange={(e) => setprice(e.target.value)} placeholder='Enter the price' />
           </div>
           <div>
-            <input type="text" placeholder='Description goes here...' />
+            <input type="text" onChange={(e) => setdesc(e.target.value)} placeholder='Description goes here...' />
           </div>
           <button type="submit">Submit</button>
         </form>
